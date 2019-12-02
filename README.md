7c77f3ce0163ae0c7ae45e865e72cec4374bfaad
Nancy4510 <nancy4510@gmail.com>
2019-12-02 12:23:48 -0500

    Added components folder named Pokemon
    Created a components folder for Pokemon to have all my create, update,
    show and delete files, as well as a Pokemon form.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # **Pokemon - Client Side**
+
+## User Stories
+- As a user, I want to be able to sign-up successfully.
+
+- As a user, I want to be able to sign-in successfully.
+
+- As a user, I want to be able to change my password successfully.
+
+- As a user, I want to be able to create a Pokemon.
+
+- As a user, I want to be able to update a Pokemon.
+
+- As a user, I want to be able to view a Pokemon.
+
+- As a user, I want to be able to delete a Pokemon.
---
+++ src/components/pokemon/CreatePokemon.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+
+const CreatePokemon = props => {
+  const [pokemon, setPokemon] = useState({ name: '', type: '', attack: '', weakness: '' })
+
+  const handleChange = event => {
+    event.persist()
+    setPokemon({ ...pokemon, [event.target.name]: event.target.value })
+  }
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    axios({
+      url: `${apiUrl}/pokemons`,
+      method: 'POST',
+      headers: {
+        // express
+        'Authorization': `Bearer ${props.user.token}`
+      },
+      data: { pokemon }
+    })
+      .then(res => {
+        props.alert({ heading: 'Success', message: 'Pokemon Created', variant: 'success' })
+        props.history.push(`pokemons/${res.data.book._id}`)
+      })
+      .catch(console.error)
+  }
+
+  return (
+
+    <PokemonForm
+      pokemon={pokemon}
+      handleChange={handleChange}
+      handleSubmit={handleSubmit}
+    />
+  )
+}
+
+export default CreatePokemon
